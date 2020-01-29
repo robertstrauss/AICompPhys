1b762584a76af5250f68f49d54bf4607b95ddfc9
ROBERT STRAUSS <robertstrauss@LAHS-COMPSCI18-16.local>
2020-01-29 09:35:49 -0700

    plotting data

--- code/lotka-volterra2.jl
+++ code/lotka-volterra2.jl
@@ -1,20 +1,35 @@
 using DifferentialEquations, Flux, Plots, DiffEqFlux, LinearAlgebra
-function lotka_volterra(du,u,p,t)
-  x,y = u
-  a, b, c, d = p
-  du[1] = dx =  a*x - b*x*y
-  du[2] = dy = -c*y + d*x*y
-end
-u0 = [1.0f0,1.0f0]
-tspan = (0.0f0,10.0f0)
+# function lotka_volterra(du,u,p,t)
+#   x,y = u
+#   a, b, c, d = p
+#   du[1] = dx =  a*x - b*x*y
+#   du[2] = dy = -c*y + d*x*y
+# end
+# u0 = [0.44249; 4.6280594]#[1.0f0,1.0f0]
+
+
+tspan = (0.0f0,16.0f0)
 t = range(tspan[1], tspan[2], length=100)
-p = [1.5f0 1.0f0 3.0f0 1.0f0]
-prob = ODEProblem(lotka_volterra,u0,tspan,p)
+# p = [1.3f0  0.9f0
+#      1.8f0  0.5f0]
+# prob = ODEProblem(lotka_volterra,u0,tspan,p)
+include("MyUtils.jl")
 
-sol = solve(prob,Tsit5(),saveat=t)
-data = Array(sol)
 
-plot(data')
+
+
+
+# truth ODE
+## 2-animal lotka-volterra
+u0 = gc(Float32[0.44249; 4.6280594])
+A = Float32[0.0 -0.9; 0.5 0.0]
+b = Float32[1.3, -1.8]
+dudt = lv(A, b)
+
+sol = neural_ode(dudt, u0, tspan, saveat=t)#solve(prob,Tsit5(),saveat=t)
+data = Array(Flux.data(sol))
+
+plot(t, data')
 
 
 struct LV
@@ -33,7 +48,7 @@ Flux.@treelike LV
 #
 # p = [1.2f0 -1.2f0; -2.7f0 0.8f0] # initial geuss
 
-dudt = Chain(LV([1.2f0; -3.3f0],[-0.7f0; 1.1f0]))
+# dudt = Chain(LV([1.2f0; -3.3f0],[-0.7f0; 1.1f0]))
 # Chain(
 #             LV,
 #             # to start, the network is the identity matrix, so the initial geuss it just the simple ODE alone.
@@ -48,10 +63,15 @@ dudt = Chain(LV([1.2f0; -3.3f0],[-0.7f0; 1.1f0]))
 #             LinNonlin(Matrix{Float32}(I, 3, 6), convert(Array{Float32,1}, zeros(3)), [0.0f0], tanh),
 #             LinNonlin(Matrix{Float32}(I, 2, 3), convert(Array{Float32,1}, zeros(2)), [0.0f0], tanh)
 # )
+dudt_train = Chain(
+    Dense(2, 32, swish),
+    Dense(32, 32, swish),
+    Dense(32, 32, swish),
+    Dense(32, 2),
+)
+ps = Flux.params(dudt_train)
 
-ps = Flux.params(dudt)
-
-n_ode = x->neural_ode(dudt,x,tspan,Tsit5(),saveat=t)#,reltol=1e-7,abstol=1e-9)
+n_ode = x->neural_ode(dudt_train,x,tspan,Tsit5(),saveat=t)#,reltol=1e-7,abstol=1e-9)
 
 pred = n_ode(u0)
 
@@ -64,9 +84,12 @@ cb = function () # callback function to observe training
     # display(("ps",ps))
     # plot current prediction against data
     cur_pred = Flux.data(predict_n_ode())
-    pl = plot(t,data',label="data")
-    # plot!(t,cur_pred[1,:]-pendulumData[1,:],label="difference",linestyle=:dot)
+    pl = Plots.plot()
+    t2 = range(tspan[1], tspan[2], length=10)
+    plot!(pl,t,data',label="data")
     scatter!(pl,t,cur_pred',label="prediction")
+    # plot!(t,cur_pred[1,:]-pendulumData[1,:],label="difference",linestyle=:dot)
+
     display(plot(pl))
 end
 
@@ -78,4 +101,4 @@ function train(iters::Int = 3; opt = ADAM(0.1), callback = cb, ps = ps, loss_n_o
     Flux.train!(loss_n_ode, ps, d, opt, cb = Flux.throttle(callback, 3))
 end
 
-train(30,opt=ADAM(0.01))
+train(30,opt=ADAM(0.0001))
